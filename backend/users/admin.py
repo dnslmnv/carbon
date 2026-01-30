cbf8a515af17f018925f41eba712139531bf4bcd
Deni <120966148+dnslmnv@users.noreply.github.com>
2026-01-30 08:07:21 +0300

    Add profile fields to user model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,6 +6,12 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (("Role", {"fields": ("role",)}),)
-    add_fieldsets = UserAdmin.add_fieldsets + (("Role", {"fields": ("role",)}),)
-    list_display = UserAdmin.list_display + ("role",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Profile", {"fields": ("name", "phone_number")}),
+        ("Role", {"fields": ("role",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Profile", {"fields": ("name", "phone_number", "email")}),
+        ("Role", {"fields": ("role",)}),
+    )
+    list_display = UserAdmin.list_display + ("name", "phone_number", "role")
